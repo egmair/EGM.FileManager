1354b929463ed372d7d322eca2a1abf2c45e5800
euanm <euangmair@gmail.com>
2022-05-28 16:42:01 +0100

    Add project files.

---
+++ EGM.FileManager/Program.cs
@@ -0,0 +1,10 @@
+using EGM.FileManager;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
