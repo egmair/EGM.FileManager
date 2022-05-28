365864aa7a42f67c3c1d60169c864d24e7aab6ae
euanm <euangmair@gmail.com>
2022-05-28 16:55:30 +0100

    Created new options library to configure the service.

---
+++ EGM.FileManager.Core.Options/FileManagerOptions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace EGM.FileManager.Core.Options
+{
+    public sealed class FileManagerOptions
+    {
+        public string? SourceDirectory { get; set; }
+
+        public Dictionary<string, string> TargetDirectoryBindings { get; set; } = new Dictionary<string, string>();
+    }
+}
